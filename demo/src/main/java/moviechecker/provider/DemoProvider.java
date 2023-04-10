de669c0e97864512b150e43243d92921ed33b07e
unkus <ukv@inbox.ru>
2023-04-10 22:46:18 +0300

    Fixes #16

--- demo/src/main/java/moviechecker/provider/DemoProvider.java
+++ demo/src/main/java/moviechecker/provider/DemoProvider.java
@@ -1,10 +1,8 @@
 package moviechecker.provider;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URI;
-import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -12,134 +10,106 @@ import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import moviechecker.model.State;
 import org.springframework.stereotype.Component;
 
-import moviechecker.model.Episode;
-import moviechecker.model.Movie;
-import moviechecker.model.Season;
-import moviechecker.model.Site;
-import moviechecker.model.State;
-import moviechecker.repository.EpisodeRepository;
-import moviechecker.repository.MovieRepository;
-import moviechecker.repository.SeasonRepository;
-import moviechecker.repository.SiteRepository;
-
+/**
+ * Example of how to extract data from a website.
+ */
 @Component
 public class DemoProvider extends AbstractMovieProvider {
 
-	@Autowired
-	private SiteRepository sites;
+    private static final String SITE = "https://some.site";
 
-	@Autowired
-	private MovieRepository movies;
+    /**
+     * Pattern for detecting a line with the following data: identifier, title, link to a movie.
+     */
+    private static final Pattern MOVIE_PATTERN = Pattern.compile("<a href=\"(?<href>/(?<pageId>.*)\\.html)\">(?<title>.*)</a>");
 
-	@Autowired
-	private SeasonRepository seasones;
+    /**
+     * Pattern for detecting a line with the following data: season number, episode number, episode title, link to episode.
+     */
+    private static final Pattern EPISODE_PATTERN = Pattern.compile("<a href=\"(?<href>.*/(?<season>\\d+)/(?<episode>\\d+)\\.html)\">(?<title>.*)</a>");
 
-	@Autowired
-	private EpisodeRepository episodes;
+    /**
+     * Pattern for detecting a line with a release data.
+     */
+    private static final Pattern DATE_PATTERN = Pattern.compile("(?<date>Новая серия в|Сегодня|Вчера|\\d{1,2}-\\d{2}-\\d{4}),? (?<time>\\d{2}:\\d{2})");
 
-	private static final String SITE = "https://some.site";
-	// /<movie>.html
-	private static final String MOVIE_PAGE_FORMAT = "/%s.html";
-	// /<movie>/<season>.html
-	private static final String SEASON_PAGE_FORMAT = "/%s/%d.html";
-	// /<movie>/<season>/<episode>.html
-	private static final String EPISODE_PAGE_FORMAT = "/%s/%d/%d.html";
+    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("d-MM-yyyy");
 
-	// @formatter:off
-	// <movie><title><a href="/first-movie.html">First movie</a></title><episode><a href="/first-movie/1/13.html">Серия 13</a></episode><date>Сегодня, 12:23</date></movie>
-	// @formatter:on
-	private static final Pattern MOVIE_TAG_PATTERN = Pattern.compile(
-			"(?:<title><a href=\"(?<movieHref>.*)\">(?<movieTitle>.*)</a></title>)(?:<episode><a href=\"(?<episodeHref>.*)\">(?<episodeTitle>.*)</a></episode>)(?:<date>(?<date>.*)</date>)");
+    /**
+     * Prepared data.html file is used as a page received from a site.
+     *
+     * @return The new reader.
+     */
+    private BufferedReader createHtmlReader() {
+        return new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/data.html"), StandardCharsets.UTF_8));
+    }
 
-	private static final Pattern MOVIE_HREF_PATTERN = Pattern.compile("/(?<pageId>.*).html");
-	private static final Pattern EPISODE_HREF_PATTERN = Pattern
-			.compile("/(?:.*)/(?<season>\\d+)/(?<episode>\\d+).html");
+    @Override
+    public void retrieveData() throws Exception {
+        URI address = URI.create(SITE);
 
-	private static final Pattern RELEASE_DATE_PATTERN = Pattern
-			.compile("(?<date>Новая серия в|Сегодня|Вчера|\\d?\\d-\\d{2}-\\d{4})(,? (?<time>\\d{2}:\\d{2}))?");
+        // Use Builder to fill data.
+        DataRecord.Builder dataRecordBuilder = new DataRecord.Builder();
+        dataRecordBuilder.site(address);
 
-	private static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("d-MM-yyyy");
+        try (BufferedReader reader = createHtmlReader()) {
+            String inputLine;
+            while ((inputLine = reader.readLine()) != null) {
 
-	private BufferedReader createHtmlReader() throws IOException {
-		return new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/data.html")));
-	}
+                Matcher movieMatcher = MOVIE_PATTERN.matcher(inputLine);
+                if (movieMatcher.find()) {
+                    dataRecordBuilder.moviePageId(movieMatcher.group("pageId"))
+                            .movieTitle(movieMatcher.group("title"))
+                            .movieLink(URI.create(movieMatcher.group("href")));
 
-	@Override
-	public void retrieveData() throws Exception {
-		URI address = URI.create(SITE);
+                    Matcher episodeMatcher = EPISODE_PATTERN.matcher(reader.readLine());
+                    if (episodeMatcher.find()) {
+                        dataRecordBuilder.seasonNumber(Integer.parseInt(episodeMatcher.group("season")))
+                                .episodeNumber(Integer.parseInt(episodeMatcher.group("episode")))
+                                .episodeTitle(episodeMatcher.group("title"))
+                                .episodeLink(URI.create(episodeMatcher.group("href")));
+                    } else {
+                        throw new Exception("Unable to parse data for episode.");
+                    }
 
-		// Находим или создаем запись для сайта
-		Site site = sites.findByAddress(address).orElse(new Site(address));
-		sites.save(site);
-
-		BufferedReader reader = createHtmlReader();
-		try {
-			String inputLine;
-			while ((inputLine = reader.readLine()) != null) {
-				Matcher m = MOVIE_TAG_PATTERN.matcher(inputLine);
-				if (m.find()) {
-					String movieHref = m.group("movieHref");
-					String movieTitle = m.group("movieTitle");
-
-					String pageId = MOVIE_HREF_PATTERN.matcher(movieHref).group("pageId");
-					Movie movie = movies.findBySiteAndPageId(site, pageId).orElse(new Movie(site, pageId));
-					movie.setTitle(movieTitle);
-					movie.setLink(URI.create(movieHref));
-					movies.save(movie);
-
-					String episodeHref = m.group("episodeHref");
-					Matcher episodeHrefMatcher = EPISODE_HREF_PATTERN.matcher(episodeHref);
-					int seasonNumber = Integer.parseInt(episodeHrefMatcher.group("season"));
-					int episodeNumber = Integer.parseInt(episodeHrefMatcher.group("episode"));
-
-					Season season = seasones.findByMovieAndNumber(movie, seasonNumber)
-							.orElse(new Season(movie, seasonNumber));
-					season.setTitle(movieTitle + " " + seasonNumber);
-					season.setLink(URI.create(inputLine));
-					seasones.save(season);
-
-					Episode episode = episodes.findBySeasonAndNumber(season, episodeNumber)
-							.orElse(new Episode(season, episodeNumber));
-					episode.setTitle(m.group("episodeTitle"));
-					episode.setLink(URI.create(episodeHref));
-
-					Matcher dateMatcher = RELEASE_DATE_PATTERN.matcher(m.group("date"));
-					if (dateMatcher.find()) {
-						LocalDate localDate;
-						String date = dateMatcher.group("date");
-						switch (date) {
-						case "Новая серия в":
-							episode.setState(State.EXPECTED);
-							localDate = LocalDate.now();
-							break;
-						case "Сегодня":
-							episode.setState(State.RELEASED);
-							localDate = LocalDate.now();
-							break;
-						case "Вчера":
-							episode.setState(State.RELEASED);
-							localDate = LocalDate.now().minusDays(1);
-							break;
-						default:
-                            episode.setState(State.RELEASED);
-                            localDate = LocalDate.parse(date, dateFormat);
+                    Matcher dateMatcher = DATE_PATTERN.matcher(reader.readLine());
+                    if (dateMatcher.find()) {
+                        LocalDate localDate;
+                        String date = dateMatcher.group("date");
+                        switch (date) {
+                            case "Новая серия в" -> {
+                                dataRecordBuilder.episodeState(State.EXPECTED);
+                                localDate = LocalDate.now();
+                            }
+                            case "Сегодня" -> {
+                                dataRecordBuilder.episodeState(State.RELEASED);
+                                localDate = LocalDate.now();
+                            }
+                            case "Вчера" -> {
+                                dataRecordBuilder.episodeState(State.RELEASED);
+                                localDate = LocalDate.now().minusDays(1);
+                            }
+                            default -> {
+                                dataRecordBuilder.episodeState(State.RELEASED);
+                                localDate = LocalDate.parse(date, dateFormat);
+                            }
                         }
-                    String time = dateMatcher.group("time");
-                    if (time != null) {
-                        episode.setDate(LocalDateTime.of(localDate, LocalTime.parse(time)));
+                        String time = dateMatcher.group("time");
+                        if (time != null) {
+                            dataRecordBuilder.episodeDate(LocalDateTime.of(localDate, LocalTime.parse(time)));
+                        }
+
+                        // Send data to database
+                        saveRecord(dataRecordBuilder.build());
+                    } else {
+                        throw new Exception("Unable to parse date.");
                     }
                 }
-
-					episodes.save(episode);
-				}
-			}
-		} finally {
-			reader.close();
-		}
-
-	}
+            }
+        }
+    }
 
 }
