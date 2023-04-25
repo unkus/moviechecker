16afc70178f51d952bddda4112fb4a79d26e0761
Kirill Yunyshev <ukv@inbox.ru>
2023-04-26 00:12:45 +0300

    SQL misprint correction

--- lib/src/main/java/moviechecker/repository/EpisodeRepository.java
+++ lib/src/main/java/moviechecker/repository/EpisodeRepository.java
@@ -16,7 +16,7 @@ public interface EpisodeRepository extends CrudRepository<Episode, Long> {
 //	@Query("SELECT e FROM Episode e WHERE e.state = :state ORDER BY e.date DESC")
 	Iterable<Episode> findAllByStateNotOrderByDateDesc(State state);
 
-//	@Query("SELECT e FROM Episode e WHERE e.season = :season and e.number = :number")
+//	@Query("SELECT e FROM Episode e WHERE e.season = :season AND e.number = :number")
 	Optional<Episode> findBySeasonAndNumber(Season season, int number);
 
 }
